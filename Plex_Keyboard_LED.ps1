2a4e6e6a74750c9acaf9333ea2730bfecb201758
Daavid-git <36458059+Daavid-git@users.noreply.github.com>
2018-02-13 22:32:29 +0100

    First commit
    First commit

---
+++ Plex_Keyboard_LED.ps1
@@ -0,0 +1,78 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2017 v5.4.142
+	 Created on:   	2017-07-25 12:29
+	 Created by:   	Daavid
+	 Organization: 	
+	 Filename:     	Plex now-playing corsair LED
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+$active = $true
+$keyboard_found = $false
+
+$Plex_token = ""
+$Plex_local_server_IP = ""
+$Check_interval_secounds = 10
+$LED_color = [System.Drawing.Color]::Orange
+
+Import-Module .\CUE.NET\CUE.NET.dll
+
+while ($keyboard_found -eq $false)
+{
+	try
+	{
+		[CUE.NET.CueSDK]::Initialize()
+		[CUE.NET.Devices.Keyboard.CorsairKeyboard]$keyboard = [CUE.NET.CueSDK]::KeyboardSDK
+		
+		if ($keyboard -ne "")
+		{
+			$keyboard_found = $true
+			Write-Host "Keyboard: $($keyboard.DeviceInfo.model)"
+		}
+	}
+	catch
+	{
+		Write-Host "Unable to initialize/get keyboard" -ForegroundColor Red
+		Start-Sleep -Seconds 10
+	}
+}
+
+$last_check = -1
+while ($active)
+{
+	try
+	{
+		$req = Invoke-WebRequest -Uri "http://$($Plex_local_server_IP):32400/status/sessions?X-Plex-Token=$Plex_token" -Method GET -ErrorAction Stop
+		[xml]$data = $req.content
+		$numbers_of_streams = $data.MediaContainer.size
+		$users = $data.MediaContainer.Video.User.title
+		$title = $data.MediaContainer.Video.grandparentTitle
+	}
+	catch
+	{
+		Write-Host "Unable to get plex data" -ForegroundColor Red
+	}
+	
+	
+	if ($numbers_of_streams -ne $last_check) #check if stream count has changed
+	{
+		[CUE.NET.CueSDK]::Reinitialize() #Reset keyboard LEDs
+		$Keypad = "Keypad$($numbers_of_streams)"
+		$keyboard[[CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardLedId]::$Keypad].Color = $LED_color
+		$keyboard.Update()
+		if ($users.count -ne 0)
+		{
+			Write-Host "Users: $users"
+		}
+	}
+	else
+	{
+		#Do nothing
+	}
+	
+	$last_check = $numbers_of_streams
+	Start-Sleep -Seconds $Check_interval_secounds
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Plex-Now-Playing-Corsair-LED
+# Plex Now-Playing Corsair LED
+
+This script will change the color on your numpad keys in order to display the current active steams on your plex server.
+Recom
+
+Before you run the Powershell script, you need to update the configuration variables in the script (.ps1 file) and make sure the Powershell execution policy is not restricted.
+
+## Update Powershell Execution Policy
+Open Powershell and run the following command:
+
+    Set-ExecutionPolicy -scope LocalMachine -ExecutionPolicy RemoteSigned
+Optimal to do this both for 64/32-bit Powershell.
+![enter image description here](https://i.imgur.com/dFkj2qS.png)
+
+
+## Update configuration variables
+
+Open *Plex_Keyboard_LED.ps1* with Powershell ISE or a text editor and update the variables.
+
+    $Plex_token = ""
+    $Plex_local_server_IP = ""
+    $Check_interval_secounds = 10
+    $LED_color = [System.Drawing.Color]::Orange
+
+### Plex Authentication token / X-Plex-Token
+You can find the Plex authentication token with by following this plex support article:
+[Finding an authentication token / X-Plex-Token](https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/) 
+
+    $Plex_token = "Add_token_here"
+
+### Plex local server IP
+Can be found in the URL. Example: http://192.168.1.200:32400/web/index.html
+
+    $Plex_local_server_IP = "192.168.1.200"
+
+## Create a scheduled Task
